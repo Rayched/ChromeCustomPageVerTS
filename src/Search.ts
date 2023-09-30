e96fd20a30d69d0fa59f20575551ad663cd10324
Rayched <ny2401@naver.com>
2023-09-30 11:27:40 +0900

    검색창 구현 시작

---
+++ src/Search.ts
@@ -0,0 +1,11 @@
+//검색창 logic
+
+const SearchInput = document.getElementById("Search_Input");
+
+const ShowSearchResult = () => {
+    if(!SearchInput) return;
+    let SearchWord = SearchInput.value;
+    //SearchInput 객체에서 'value'라는 property 참조하지 못하는 error 발생
+    
+    window.location.href="https://www.google.com/search?q={SearchWord}";
+};
