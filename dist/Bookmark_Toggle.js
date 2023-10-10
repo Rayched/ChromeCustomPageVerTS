03c77c3b456e72a3323ae3873337d0d810ca6818
Rayched <ny2401@naver.com>
2023-10-10 16:13:21 +0900

    북마크 아이템 추가하기 기본 틀 작성

--- dist/Bookmark_Toggle.js
+++ dist/Bookmark_Toggle.js
@@ -13,6 +13,32 @@ const BookmarkClose = document.getElementById("Bookmark_Close");
 const BookmarkOpenBtn = document.getElementById("Bookmark_Open_Btn");
 const BookmarkCloseBtn = document.getElementById("Bookmark_Close_Btn");
 /*
+    const BookmarkOpenBtn: HTMLElement = document.getElementById("Bookmark_Open_Btn");
+    => 'BookmarkOpenBtn' is possibly 'null' 경고 발생
+    이게 안되는 이유에 대한 나름대로의 고찰
+
+    const BookmarkOpenBtn: HTMLElement = document.getElementById("Bookmark_Open_Btn") as HTMLElement;
+    => 이거는 된다.
+    => getElementById(id: string) 메서드의 return 값의 타입을 HTMLElement라고 단언을 내렸기에
+       HTMLElement 타입으로 선언한 변수 BookmarkOpenBtn에 할당을 해도 문제가 안생김
+       근데 이건 너무 빡빡한 느낌이다.
+       그냥 타입 단언만 해두자.
+*/
+//getElementById(Id: string)
+//문자열 id 요소가 인자로 전달되면
+//해당 메서드는 HTMLElement 또는 null을 return하게된다.
+//즉, 변수 타입을 HTMLElement로 선언을 해도
+//전달되는 값인 getElementById(Id: string)의 return이 
+//HTMLElement 외에 null을 반환할 수도 있기에
+//경고가 발생하게된다.
+//(id 값과 일치하는 HTML 요소가 있다면 HTMLElement를, 없다면 null을 return 하게된다.)
+//컴파일러 입장에서는 해당 id 값을 가진 요소가 있을지 없을지를 판단하지는 못하기에
+//이거 null일수도 있다는 경고를 띄우게된다.
+//그렇기에 해당 id 값을 가진 요소가 있다는 것을 우리는 알고있기 때문에
+//'as'를 사용해서 return 값이 HTMLElement라고 단언을 해줘야한다.
+//즉, 컴파일러한테 이 놈이 HTMLElement 타입이라는 것을 내가 보증한다는 형태의 정보를 넘겨주는 것이다.
+//그리고 그 정보를 넘겨받은 컴파일러가 이걸 가지고 걸고 넘어지지 않게되는 것이다.
+/*
     * [북마크 열기]를 클릭하면, 북마크 바를 열고
       [북마크 닫기]를 클릭하면, 북마크 바를 닫는 동작을 수행하는
       BookmarkBarToggle 함수
@@ -48,6 +74,5 @@ const BookmarkBarToggle = () => {
         BookmarkClose.style.display = "none";
     }
 };
-
 BookmarkOpenBtn.addEventListener("click", BookmarkBarToggle);
 BookmarkCloseBtn.addEventListener("click", BookmarkBarToggle);
